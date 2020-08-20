986e6d25b7318c08ed5b83798a29043114cd81a4
sweety.sivasankar <sweety.sivasankar@MYSHEZ343083D>
2020-08-20 21:41:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  albums: any;
+  allPhotos: any;
+  albumClicked = false;
+  photoClicked = false;
+  photos = [];
+  userIds: any;
+  albumIds: any;
+
+  constructor (private http: HttpClient) {
+    this.http.get<any>('http://jsonplaceholder.typicode.com/albums').subscribe(data => {
+      this.albums = data;
+    })
+    this.http.get<any>('http://jsonplaceholder.typicode.com/photos').subscribe(data => {
+      this.allPhotos = data;
+    })
+
+  }
+
+  showPhotos(userId: any) {
+    this.albumClicked = true;
+    for (let photo of this.allPhotos) { 
+      if (userId.id === photo.albumId) {
+        this.photos.push(photo);
+      }
+    }
+  }
   title = 'app';
 }
